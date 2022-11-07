33ffa632219a47cb4ad348cc7dcb59a853be610b
行勇 <wangyanqi.wyq@alibaba-inc.com>
2022-11-07 20:46:41 +0800

    [1.0.0][feat]创建工程

---
+++ QixinCooperationAgency/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  QixinCooperationAgency
+//
+//  Created by 𝘼𝙘𝙤𝙛𝙛𝙚𝙧 on 2022/11/7.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
